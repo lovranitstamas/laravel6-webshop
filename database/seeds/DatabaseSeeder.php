b8705c8646e54222c58ebc696707f343d93235c0
lovranitstamas <tamas198601@gmail.com>
2021-04-11 20:49:57 +0200

    add default admin

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\User;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,5 +14,13 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $user = new User();
+        $user->name = "admin";
+        $user->email = "admin@admin.hu";
+        $user->password = \Hash::make("admin");
+        $user->created_at = Carbon::now()->format('Y-m-d H:i:s');
+        $user->updated_at = Carbon::now()->format('Y-m-d H:i:s');
+
+        $user->save();
     }
 }
