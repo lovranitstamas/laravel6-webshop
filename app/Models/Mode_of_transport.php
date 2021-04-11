06dd585d19c5521452a4a583d5d79ac01ce38db0
lovranitstamas <tamas198601@gmail.com>
2021-04-11 20:55:01 +0200

    remove the extra cost of transport [admin]

--- app/Models/Mode_of_transport.php
+++ app/Models/Mode_of_transport.php
@@ -12,6 +12,11 @@ class Mode_of_transport extends Model
     public function setAttributes($data)
     {
         $this->mode_hu = $data['mode_hu'];
+        if (isset($data['extra_cost']) && $data['extra_cost']) {
+            $this->extra_cost = $data['extra_cost'];
+        } else {
+            $this->extra_cost = null;
+        }
     }
 
     /*public function product()
@@ -27,6 +32,6 @@ class Mode_of_transport extends Model
             'mode_of_transport_id',
             'product_id')
             ->withTimestamps();
-            //->withPivot(['weight']);
+        //->withPivot(['weight']);
     }
 }
